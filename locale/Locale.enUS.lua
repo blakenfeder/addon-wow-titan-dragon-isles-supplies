cec925c93dc71dbea43096383b7676e3ffec9703
Blakenfeder <git@blakenfeder.com>
2023-01-16 16:05:42 -0300

    Major refactor and improved lang support
    This commit refactors the code to retrieve the currency by its ID
    instead of searching for it within the known currencies by icon.
    
    It also automatically gets the currency name, description, and quality
    from the currency info object, not requiring to specify the name and
    description in the translations anymore. This way, the addOn now
    partialy supports all languages that WoW supports (just a few
    translations will still need to be added manually).
    
    Finally, missing esMX translations are being added.

--- locale/Locale.enUS.lua
+++ locale/Locale.enUS.lua
@@ -1,10 +1,6 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("DragonIslesSupplies","enUS",true)
 
-L["BKFD_TITAN_DIR_BUTTON_LABEL"] = "Dragon Isles Supplies: ";
-L["BKFD_TITAN_DIR_MENU_TEXT"] = "Dragon Isles Supplies";
-L["BKFD_TITAN_DIR_TOOLTIP_TITLE"] = "Dragon Isles Supplies";
-L["BKFD_TITAN_DIR_TOOLTIP_DESCRIPTION"] = "Various resources and building\nmaterials that can assist the friendly\nfactions of the Dragon Isles in their\nfight against the Primalists. Used to\npurchase items unlocked from renown.";
-L["BKFD_TITAN_DIR_TOOLTIP_COUNT_LABEL_TOTAL"] = "Total: ";
-L["BKFD_TITAN_DIR_TOOLTIP_COUNT_LABEL_TOTAL_MAXIMUM"] = "Total Maximum: ";
-L["BKFD_TITAN_DIR_TOOLTIP_COUNT_LABEL_TOTAL_SEASONAL"] = "Season Total Earned: ";
-L["BKFD_TITAN_DIR_TOOLTIP_NOT_YET_DISCOVERED"] = "Currency not yet discovered.";
+L["BKFD_TITAN_TOOLTIP_COUNT_LABEL_TOTAL"] = "Total: ";
+L["BKFD_TITAN_TOOLTIP_COUNT_LABEL_TOTAL_MAXIMUM"] = "Total Maximum: ";
+L["BKFD_TITAN_TOOLTIP_COUNT_LABEL_TOTAL_SEASONAL"] = "Season Maximum: ";
+L["BKFD_TITAN_TOOLTIP_NOT_YET_DISCOVERED"] = "Currency not yet discovered.";
